bbd9f02cfb6815cbccf71a7dc67e16e6ecd0b494
caszxc <cjohnkenneth35@gmail.com>
2026-03-17 19:11:59 +0800

    Upload: Serial Number Removed

--- backend/routes/student.routes.js
+++ backend/routes/student.routes.js
@@ -417,7 +417,6 @@ router.post("/uploadStudents", upload.single("file"), async (req, res) => {
       // Check required fields
       const requiredFields = [
         "card_id_control_number",
-        "card_serial_number",
         "card_type",
         "student_number",
         "first_name",
@@ -436,10 +435,6 @@ router.post("/uploadStudents", upload.single("file"), async (req, res) => {
         isNaN(parseInt(row.card_id_control_number))
       ) {
         rowErrors.push("card_id_control_number must be a number");
-      }
-
-      if (row.card_serial_number && isNaN(parseInt(row.card_serial_number))) {
-        rowErrors.push("card_serial_number must be a number");
       }
 
       if (
@@ -485,7 +480,7 @@ router.post("/uploadStudents", upload.single("file"), async (req, res) => {
         const cleanedRow = {
           card_id_control_number: parseInt(row.card_id_control_number),
           date_enrolled: row.date_enrolled || null,
-          card_serial_number: parseInt(row.card_serial_number),
+          card_serial_number: null, //default to null since it's not required and may not be provided
           card_type: row.card_type.toString().trim(),
           student_number: row.student_number.toString().trim(),
           first_name: row.first_name.toString().trim(),
