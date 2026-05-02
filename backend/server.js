5fe884113c75a08a4fc75cf821769ea19040f3a5
Kurt <kurtkurt05pantaleon@gmail.com>
2026-05-02 18:58:07 +0800

    for dev mode

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,11 @@ const seedCourses = require("./seeds/courseSeed");
 const app = express();
 
 //-unccomment kapag dev mode
-// const PORT = 3001;
+const PORT = 3001;
 
 //-uncomment kapag deploy locally
-const PORT = process.env.PORT || 3005;
-const HOST = process.env.HOST || "0.0.0.0";
+// const PORT = process.env.PORT || 3005;
+// const HOST = process.env.HOST || "0.0.0.0";
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
@@ -24,24 +24,21 @@ app.use("/accounts", require("./routes/accountManagement.route"));
 app.use("/settings", require("./routes/settings.route"));
 
 // Sync DB and seed on startup
-// db.sequelize.sync({ alter: true }).then(async () => {
-//   console.log("Database synced");
-//   await seedCourses();
-// });
+
 db.sequelize.sync().then(async () => {
   console.log("Database synced");
   await seedCourses();
 });
 
 //uncomment kapag dev mode
-// app.listen(PORT, () => {
-//   console.log(`Backend running at http://localhost:${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Backend running at http://localhost:${PORT}`);
+});
 
 //uncomment kapag deploy locally
-app.listen(PORT, HOST, () => {
-  console.log(`Backend running at http://${HOST}:${PORT}]`);
-});
+// app.listen(PORT, HOST, () => {
+//   console.log(`Backend running at http://${HOST}:${PORT}]`);
+// });
 
 // app.listen(PORT, "0.0.0.0", () => {
 //   console.log("Backend running");
--- frontend/src/Api/baseUrl.js
+++ frontend/src/Api/baseUrl.js
@@ -1,2 +1,5 @@
 export const BASE_URL = "http://localhost:3001";
+
+//uncomment kapag deploy locally, comment out kapag dev mode
 // export const BASE_URL = "http://192.168.254.146:3001";
+// export const BASE_URL = import.meta.env.VITE_API_URL;
