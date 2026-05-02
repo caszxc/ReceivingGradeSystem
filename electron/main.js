f8f77b3ceed1b3c0a91e59628723ad60fca6222b
JhonnBretana <jhonnbretz@gmail.com>
2026-05-02 20:16:51 +0800

    Refactor code structure for improved readability and maintainability

--- electron/main.js
+++ electron/main.js
@@ -13,7 +13,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: path.join(__dirname, "icon", "PLVLogo.ico"),
+    icon: path.join(__dirname, "icon", "PLVLogos.ico"),
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       contextIsolation: true,
