2b19d5f3edefca09a4f8fd1a6ffe7c7c9708d559
JhonnBretana <jhonnbretz@gmail.com>
2026-05-02 19:55:39 +0800

    Refactor server and Electron main files for local deployment configuration

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,11 @@ const seedCourses = require("./seeds/courseSeed");
 const app = express();
 
 //-unccomment kapag dev mode
-const PORT = 3001;
+// const PORT = 3001;
 
 //-uncomment kapag deploy locally
-// const PORT = process.env.PORT || 3005;
-// const HOST = process.env.HOST || "0.0.0.0";
+const PORT = process.env.PORT || 3005;
+const HOST = process.env.HOST || "0.0.0.0";
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
@@ -33,18 +33,13 @@ db.sequelize.sync().then(async () => {
 });
 
 //uncomment kapag dev mode
-app.listen(PORT, () => {
-  console.log(`Backend running at http://localhost:${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Backend running at http://localhost:${PORT}`);
+// });
+
+//uncomment kapag deploy locally
+app.listen(PORT, HOST, () => {
+  console.log(`Backend running at http://${HOST}:${PORT}]`);
 });
-
-//uncomment kapag deploy locally
-// app.listen(PORT, HOST, () => {
-//   console.log(`Backend running at http://${HOST}:${PORT}]`);
-// });
-
-//uncomment kapag deploy locally
-// app.listen(PORT, HOST, () => {
-//   console.log(`Backend running at http://${HOST}:${PORT}]`);
-// });
 
 module.exports = app;
